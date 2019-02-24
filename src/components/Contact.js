a015858abec0cba6abef354d59c3a2b474fe2aff
Ryan Ingram <ryani@dpslink.com>
2019-02-24 16:45:42 -0500

    updated footer with real address phone and email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,22 +28,22 @@ const Contact = (props) => (
                     <div className="contact-method">
                         <span className="icon alt fa-envelope"></span>
                         <h3>Email</h3>
-                        <a href="#">information@untitled.tld</a>
+                        <a href="#">alorisoftware@gmail.com</a>
                     </div>
                 </section>
                 <section>
                     <div className="contact-method">
                         <span className="icon alt fa-phone"></span>
                         <h3>Phone</h3>
-                        <span>(000) 000-0000 x12387</span>
+                        <span>(765)205-2512</span>
                     </div>
                 </section>
                 <section>
                     <div className="contact-method">
                         <span className="icon alt fa-home"></span>
                         <h3>Address</h3>
-                        <span>1234 Somewhere Road #5432<br />
-                        Nashville, TN 00000<br />
+                        <span>101 W Main Street<br />
+                        Crawfordsville, IN 47933<br />
                         United States of America</span>
                     </div>
                 </section>
